79309c0604d31511048a28e5b139d30de1796415
Elshan's Projects <89624267+Elshan-Projects@users.noreply.github.com>
2024-03-09 20:48:49 +0400

    Added a dot to highlight the clicked area

--- CIEDE_Algorithm_Elshan_Naghizade.py
+++ CIEDE_Algorithm_Elshan_Naghizade.py
@@ -11,16 +11,17 @@ def click_event(event, x, y, flags, params):
         highlighted_img = src.copy()
         highlighted_img[mask] = [0, 255, 0] 
 
+        cv2.circle(highlighted_img, (x, y), radius=5, color=(0, 0, 255), thickness=-1)
         base_name = image_path.rsplit('.', 1)[0]  
         result_path = f"{base_name} x-{x} y-{y} threshold-{threshold}.jpg"
         cv2.imwrite(result_path, highlighted_img)
 
+        # Display the result
         cv2.imshow('Highlighted Image', highlighted_img)
 
 def closeness_driver(image_path, threshold=10):
     src = cv2.imread(image_path)
     lab_image = cv2.cvtColor(src, cv2.COLOR_BGR2Lab)
-
     cv2.imshow('Original Image', src)
     params = [src, lab_image, threshold, image_path]
     cv2.setMouseCallback('Original Image', click_event, params)
@@ -28,6 +29,7 @@ def closeness_driver(image_path, threshold=10):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 # Provide an image path to the driver function (setting a threshold is optional)
-image_path = 'image_3.jpg'
+image_path = 'image_1.jpg'
 closeness_driver(image_path)
